6945f4eb4b0134eb7c8b171363d9d5cd3b31ebd5
Jerome Leclanche <jerome@leclan.ch>
2023-01-31 11:20:31 +0100

    Make add node button its own component

--- src/app/flow-editor/CustomNode.tsx
+++ src/app/flow-editor/CustomNode.tsx
@@ -1,5 +1,5 @@
 import { PlusCircleIcon } from "@heroicons/react/24/outline";
-import { memo } from "react";
+import React, { memo } from "react";
 import {
 	applyEdgeChanges,
 	applyNodeChanges,
@@ -10,9 +10,62 @@ import {
 	useReactFlow,
 } from "reactflow";
 
-const CustomNode = ({ id, data, isConnectable, xPos, yPos }: NodeProps) => {
+const AddNodeButton: React.FC<{ id: string; xPos: number; yPos: number }> = ({
+	id,
+	xPos,
+	yPos,
+}) => {
 	const { setNodes, setEdges } = useReactFlow();
 
+	return (
+		<button
+			type="button"
+			className="relative w-7 h-7"
+			onClick={() => {
+				const edgeChanges: EdgeAddChange[] = [];
+
+				setNodes((nodes) => {
+					const newId = `node-${nodes.length}`;
+					edgeChanges.push({
+						type: "add",
+						item: {
+							id: `edge-${id}-${newId}`,
+							source: id,
+							target: newId,
+						},
+					});
+
+					return applyNodeChanges(
+						[
+							{
+								type: "add",
+								item: {
+									id: newId,
+									position: { x: xPos, y: yPos + 100 },
+									data: { label: "New Screen" },
+									type: "custom",
+								},
+							},
+						],
+						nodes
+					);
+				});
+
+				setEdges((edges) => applyEdgeChanges(edgeChanges, edges));
+			}}
+		>
+			<PlusCircleIcon className="w-7 h-7 bottom-0 bg-white hover:bg-gray-100 rounded-full absolute" />
+		</button>
+	);
+};
+
+const CustomNode: React.FC<NodeProps> = ({
+	id,
+	data,
+	isConnectable,
+	xPos,
+	yPos,
+}) => {
 	return (
 		<div className="flex flex-col bg-white border border-black p-5 rounded-md shadow-md font-semibold group w-[150px] text-center">
 			<Handle
@@ -29,44 +82,7 @@ const CustomNode = ({ id, data, isConnectable, xPos, yPos }: NodeProps) => {
 				style={{ visibility: "hidden" }}
 			/>
 			<div className="mx-auto h-0 hidden group-hover:block">
-				<button
-					type="button"
-					className="relative w-7 h-7"
-					onClick={(e) => {
-						const edgeChanges: EdgeAddChange[] = [];
-
-						setNodes((nodes) => {
-							const newId = `node-${nodes.length}`;
-							edgeChanges.push({
-								type: "add",
-								item: {
-									id: `edge-${id}-${newId}`,
-									source: id,
-									target: newId,
-								},
-							});
-
-							return applyNodeChanges(
-								[
-									{
-										type: "add",
-										item: {
-											id: newId,
-											position: { x: xPos, y: yPos + 100 },
-											data: { label: "New Screen" },
-											type: "custom",
-										},
-									},
-								],
-								nodes
-							);
-						});
-
-						setEdges((edges) => applyEdgeChanges(edgeChanges, edges));
-					}}
-				>
-					<PlusCircleIcon className="w-7 h-7 bottom-0 bg-white hover:bg-gray-100 rounded-full absolute" />
-				</button>
+				<AddNodeButton id={id} xPos={xPos} yPos={yPos} />
 			</div>
 		</div>
 	);
