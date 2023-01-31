192714dafddfe03ad1eb03e1a7153656daa90147
Jerome Leclanche <jerome@leclan.ch>
2023-01-31 11:14:01 +0100

    Auto-connect nodes

--- src/app/flow-editor/CustomNode.tsx
+++ src/app/flow-editor/CustomNode.tsx
@@ -1,52 +1,55 @@
 import { PlusCircleIcon } from "@heroicons/react/24/outline";
 import { memo } from "react";
 import {
+	applyEdgeChanges,
 	applyNodeChanges,
+	EdgeAddChange,
+	Handle,
 	NodeProps,
 	Position,
-	useNodes,
 	useReactFlow,
 } from "reactflow";
 
-/* Handles:
-	<Handle
-		type="target"
-		position={targetPosition}
-		isConnectable={isConnectable}
-	/>
-	<Handle
-		type="source"
-		position={sourcePosition}
-		isConnectable={isConnectable}
-	/>
-*/
-
-const CustomNode = ({
-	data,
-	isConnectable,
-	xPos,
-	yPos,
-	targetPosition = Position.Top,
-	sourcePosition = Position.Bottom,
-}: NodeProps) => {
-	const nodes = useNodes();
-	const { project, setNodes } = useReactFlow();
+const CustomNode = ({ id, data, isConnectable, xPos, yPos }: NodeProps) => {
+	const { setNodes, setEdges } = useReactFlow();
 
 	return (
 		<div className="flex flex-col bg-white border border-black p-5 rounded-md shadow-md font-semibold group w-[150px] text-center">
+			<Handle
+				type="target"
+				position={Position.Top}
+				isConnectable={isConnectable}
+			/>
 			{data?.label}
+			<Handle
+				type="source"
+				position={Position.Bottom}
+				isConnectable={isConnectable}
+			/>
 			<div className="mx-auto h-0 hidden group-hover:block">
 				<button
 					type="button"
 					className="relative w-7 h-7"
 					onClick={(e) => {
-						setNodes((nodes) =>
-							applyNodeChanges(
+						const edgeChanges: EdgeAddChange[] = [];
+
+						setNodes((nodes) => {
+							const newId = `node-${nodes.length}`;
+							edgeChanges.push({
+								type: "add",
+								item: {
+									id: `edge-${id}-${newId}`,
+									source: id,
+									target: newId,
+								},
+							});
+
+							return applyNodeChanges(
 								[
 									{
 										type: "add",
 										item: {
-											id: `node-${nodes.length}`,
+											id: newId,
 											position: { x: xPos, y: yPos + 100 },
 											data: { label: "New Screen" },
 											type: "custom",
@@ -54,8 +57,10 @@ const CustomNode = ({
 									},
 								],
 								nodes
-							)
-						);
+							);
+						});
+
+						setEdges((edges) => applyEdgeChanges(edgeChanges, edges));
 					}}
 				>
 					<PlusCircleIcon className="w-7 h-7 bottom-0 bg-white hover:bg-gray-100 rounded-full absolute" />
