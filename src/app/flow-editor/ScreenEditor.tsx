6327cd94da007b70c91686e59c448baff8a963cb
Jerome Leclanche <jerome@leclan.ch>
2023-02-03 11:26:58 +0100

    Prevent dragging without using handle

--- src/app/flow-editor/ScreenEditor.tsx
+++ src/app/flow-editor/ScreenEditor.tsx
@@ -88,78 +88,85 @@ const FormComponentRow: React.FC<{
 	isEditing: boolean;
 	onDelete: () => void;
 	onEdit: (value: string) => void;
-}> = ({ component, index, isEditing, onDelete, onEdit }) => (
-	<div
-		key={index}
-		className={clsx(
-			"flex w-full p-2 my-1",
-			isEditing ? "border-2 border-gray-100 cursor-text hover:bg-gray-100" : ""
-		)}
-		draggable={isEditing}
-		onDragEnd={(e) => {
-			console.log("TODO - drag & drop", e);
-		}}
-	>
-		{isEditing ? <DragHandle /> : null}
-		{component.type === FormComponentType.Heading ? (
-			<h1 className="font-bold text-xl w-full">
-				{isEditing ? (
+}> = ({ component, index, isEditing, onDelete, onEdit }) => {
+	const [canDrag, setCanDrag] = useState(false);
+
+	return (
+		<div
+			key={index}
+			className={clsx(
+				"flex w-full p-2 my-1",
+				isEditing
+					? "border-2 border-gray-100 cursor-text hover:bg-gray-100"
+					: ""
+			)}
+			draggable={isEditing && canDrag}
+			onDragEnd={(e) => {
+				console.log("TODO - drag & drop", e);
+				setCanDrag(false);
+			}}
+		>
+			{isEditing ? <DragHandle setDragging={setCanDrag} /> : null}
+			{component.type === FormComponentType.Heading ? (
+				<h1 className="font-bold text-xl w-full">
+					{isEditing ? (
+						<input
+							type="text"
+							value={component.label}
+							className="w-full bg-none border-none text-xl"
+							placeholder="Click to edit"
+							onChange={(e) => onEdit(e.currentTarget.value)}
+						/>
+					) : (
+						component.label
+					)}
+				</h1>
+			) : component.type === FormComponentType.Text ? (
+				<p className="w-full">
+					{isEditing ? (
+						<textarea
+							value={component.label}
+							cols={60}
+							className="w-full bg-transparent border-none"
+							onChange={(e) => onEdit(e.currentTarget.value)}
+						/>
+					) : (
+						component.label
+					)}
+				</p>
+			) : component.type === FormComponentType.TextInput ? (
+				<div className="flex flex-col w-full">
 					<input
 						type="text"
+						className="w-full"
 						value={component.label}
-						className="w-full bg-none border-none text-xl"
-						placeholder="Click to edit"
-						onChange={(e) => onEdit(e.currentTarget.value)}
+						onChange={(e) => {
+							if (isEditing) onEdit(e.currentTarget.value);
+						}}
 					/>
-				) : (
-					component.label
-				)}
-			</h1>
-		) : component.type === FormComponentType.Text ? (
-			<p className="w-full">
-				{isEditing ? (
-					<textarea
-						value={component.label}
-						cols={60}
-						className="w-full bg-transparent border-none"
-						onChange={(e) => onEdit(e.currentTarget.value)}
-					/>
-				) : (
-					component.label
-				)}
-			</p>
-		) : component.type === FormComponentType.TextInput ? (
-			<div className="flex flex-col w-full">
-				<input
-					type="text"
-					className="w-full"
-					value={component.label}
-					onChange={(e) => {
-						if (isEditing) onEdit(e.currentTarget.value);
-					}}
-				/>
-			</div>
-		) : component.type === FormComponentType.Continue ? (
-			<Button variant="primary">
-				{isEditing ? (
-					<input
-						type="text"
-						value={component.label}
-						className="bg-transparent border-none p-0"
-						placeholder="Click to edit"
-						onChange={(e) => onEdit(e.currentTarget.value)}
-						size={component.label.length}
-					/>
-				) : (
-					component.label
-				)}
-			</Button>
-		) : null}
-		{isEditing && component.type !== FormComponentType.Continue ? (
-			<DeleteComponentButton onClick={onDelete} />
-		) : null}
-	</div>
-);
+				</div>
+			) : component.type === FormComponentType.Continue ? (
+				<Button variant="primary">
+					{isEditing ? (
+						<input
+							type="text"
+							value={component.label}
+							className="bg-transparent border-none p-0"
+							placeholder="Click to edit"
+							onChange={(e) => onEdit(e.currentTarget.value)}
+							size={component.label.length}
+						/>
+					) : (
+						component.label
+					)}
+				</Button>
+			) : null}
+			{isEditing && component.type !== FormComponentType.Continue ? (
+				<DeleteComponentButton onClick={onDelete} />
+			) : null}
+		</div>
+	);
+};
 
 export const ScreenEditor: React.FC<{
 	components: FormComponentRow[];
