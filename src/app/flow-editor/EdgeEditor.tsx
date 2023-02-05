2d0a5adf1312d2e7e65dfcb45f90010a81630a9d
Jerome Leclanche <jerome@leclan.ch>
2023-02-05 11:12:41 +0100

    Add more functionality around conditions

--- src/app/flow-editor/EdgeEditor.tsx
+++ src/app/flow-editor/EdgeEditor.tsx
@@ -1,12 +1,39 @@
-import { PlusSmallIcon } from "@heroicons/react/24/outline";
+import { PlusSmallIcon, TrashIcon } from "@heroicons/react/24/outline";
 import React, { useState } from "react";
 import { Edge } from "reactflow";
 import Button from "./Button";
 
-interface Condition {}
+interface Condition {
+	operation: "" | "eq" | "neq" | "gt" | "lt" | "in" | "nin";
+	input: string;
+	inputType: "string" | "number";
+	value: string;
+}
 
 const ConditionEntry: React.FC<{ condition: Condition }> = ({ condition }) => (
-	<li>Condition</li>
+	<li className="flex justify-between items-center border-b border-gray-300 py-2">
+		<div className="flex items-center">
+			<input className="w-24 mr-2" type="text" placeholder="Input" />
+			<select className="mr-2">
+				<option value="eq">equals</option>
+				<option value="neq">does not equal</option>
+
+				{condition.inputType === "number" && (
+					<>
+						<option value="gt">is more than</option>
+						<option value="lt">is less than</option>
+					</>
+				)}
+
+				<option value="in">is in</option>
+				<option value="nin">is not in</option>
+			</select>
+			<input className="w-24" type="text" placeholder="Value" />
+		</div>
+		<button className="p-1">
+			<TrashIcon className="w-5 h-5 ml-3 text-gray-400" />
+		</button>
+	</li>
 );
 
 const EdgeEditor: React.FC<{ edge: Edge; onClose: () => void }> = ({
@@ -21,7 +48,7 @@ const EdgeEditor: React.FC<{ edge: Edge; onClose: () => void }> = ({
 				className="bg-gray-600 opacity-50 w-full h-full fixed"
 				onClick={onClose}
 			></div>
-			<div className="bg-white rounded-md w-64 min-h-72 opacity-100 z-10 p-5 flex flex-col justify-between">
+			<div className="bg-white rounded-md min-w-[18rem] min-h-72 opacity-100 z-10 p-5 flex flex-col justify-between">
 				<h1 className="text-xl font-semibold">Add conditions</h1>
 
 				<ul>
@@ -34,10 +61,15 @@ const EdgeEditor: React.FC<{ edge: Edge; onClose: () => void }> = ({
 					<Button
 						variant="plain"
 						className="w-full my-3"
-						onClick={() => setConditions([...conditions, {}])}
+						onClick={() =>
+							setConditions([
+								...conditions,
+								{ operation: "", input: "", inputType: "number", value: "" },
+							])
+						}
 					>
 						<PlusSmallIcon className="w-6 h-6 mr-1" />
-						Add condition
+						New condition
 					</Button>
 
 					<div className="flex justify-between">
