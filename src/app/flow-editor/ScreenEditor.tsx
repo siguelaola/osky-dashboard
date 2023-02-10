2b58e810d73eadcef407837d3d5121481c19e366
Jerome Leclanche <jerome@leclan.ch>
2023-02-10 19:57:07 +0100

    Refactor; better checkboxes

--- src/app/flow-editor/ScreenEditor.tsx
+++ src/app/flow-editor/ScreenEditor.tsx
@@ -9,18 +9,20 @@ import React, { useState } from "react";
 import Button from "./Button";
 import DeleteComponentButton from "./DeleteComponentButton";
 import DragHandle from "./DragHandle";
-import { FormComponent as FormComponentRow, FormComponentType } from "./types";
+import {
+	FormComponent as FormComponentRow,
+	FormComponentType,
+	FormInputChoice,
+} from "./types";
 
 const AddComponentChoice: React.FC<{
-	label: string;
-	type: FormComponentType;
-	addComponent: (value: FormComponentRow) => void;
+	addComponent: () => void;
 	children: string;
-}> = ({ label, type, addComponent, children }) => (
+}> = ({ addComponent, children }) => (
 	<li className="flex w-full">
 		<button
 			className="flex w-full p-2 hover:bg-gray-100"
-			onClick={() => addComponent({ label, type })}
+			onClick={() => addComponent()}
 		>
 			{children}
 		</button>
@@ -55,30 +57,54 @@ const AddComponentButton: React.FC<{
 				{menuOpen ? (
 					<ul className="z-10 bg-white divide-y divide-gray-100 rounded-lg w-44 absolute top-12 shadow-lg py-2 border">
 						<AddComponentChoice
-							type={FormComponentType.Heading}
-							addComponent={addComponent}
-							label="New heading"
+							addComponent={() =>
+								addComponent({
+									type: FormComponentType.Heading,
+									label: "New heading",
+								})
+							}
 						>
 							Heading
 						</AddComponentChoice>
 						<AddComponentChoice
-							type={FormComponentType.Text}
-							addComponent={addComponent}
-							label="Click to edit text"
+							addComponent={() =>
+								addComponent({
+									type: FormComponentType.Text,
+									label: "New text",
+								})
+							}
 						>
 							Text
 						</AddComponentChoice>
 						<AddComponentChoice
-							type={FormComponentType.TextInput}
-							addComponent={addComponent}
-							label="(default value)"
+							addComponent={() =>
+								addComponent({
+									type: FormComponentType.TextInput,
+									label: "New text input",
+								})
+							}
 						>
 							Text input
 						</AddComponentChoice>
 						<AddComponentChoice
-							type={FormComponentType.Checkboxes}
-							addComponent={addComponent}
-							label="Click to edit label"
+							addComponent={() =>
+								addComponent({
+									type: FormComponentType.Checkboxes,
+									label: "(edit label)",
+									choices: [
+										{
+											label: "Choice 1",
+											value: "Choice 1",
+											defaultChecked: false,
+										},
+										{
+											label: "Choice 2",
+											value: "Choice 2",
+											defaultChecked: false,
+										},
+									],
+								})
+							}
 						>
 							Choices (Checkboxes)
 						</AddComponentChoice>
@@ -87,6 +113,162 @@ const AddComponentButton: React.FC<{
 			</button>
 		</>
 	);
+};
+
+const CheckboxesChoiceEdit: React.FC<{
+	choice: FormInputChoice;
+	onEdit: (label: string, checked: boolean) => void;
+	onEnter: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+}> = ({ choice, onEdit, onEnter }) => {
+	const [label, setLabel] = useState(choice.label);
+	const [checked, setChecked] = useState(choice.defaultChecked || false);
+
+	return (
+		<li className="flex items-center">
+			<input
+				type="checkbox"
+				checked={choice.defaultChecked}
+				onChange={(e) => {
+					setChecked(e.currentTarget.checked);
+					onEdit(label, e.currentTarget.checked);
+				}}
+				className="mr-2"
+			/>
+			<input
+				type="text"
+				value={label}
+				onChange={(e) => {
+					setLabel(e.currentTarget.value);
+					onEdit(e.currentTarget.value, checked);
+				}}
+				onKeyDown={(e) => {
+					console.log("keydown");
+
+					if (e.key === "Enter") {
+						console.log("enter");
+						onEnter(e);
+					}
+				}}
+				className="bg-transparent border-none p-0"
+			/>
+		</li>
+	);
+};
+
+const ComponentEditor: React.FC<{
+	component: FormComponentRow;
+	onEdit: (value: Record<string, any>) => void;
+}> = ({ component, onEdit }) => {
+	switch (component.type) {
+		case FormComponentType.Heading:
+			return (
+				<h2 className="font-bold text-xl w-full">
+					<input
+						type="text"
+						value={component.label}
+						className="w-full bg-transparent border-none text-xl"
+						placeholder="Click to edit"
+						onChange={(e) => onEdit({ label: e.target.value })}
+					/>
+				</h2>
+			);
+		case FormComponentType.Text:
+			return (
+				<textarea
+					value={component.label}
+					cols={60}
+					className="w-full bg-transparent border-none"
+					onChange={(e) => onEdit({ label: e.currentTarget.value })}
+				/>
+			);
+		case FormComponentType.TextInput:
+			return <input type="text" value={component.label} className="w-full" />;
+		case FormComponentType.Checkboxes:
+			return (
+				<ul className="w-full">
+					{component.choices?.map((choice, index) => (
+						<CheckboxesChoiceEdit
+							key={index}
+							choice={choice}
+							onEdit={(label, checked) => {
+								const newChoices = [...component.choices!];
+								newChoices[index] = {
+									...newChoices[index],
+									label,
+									defaultChecked: checked,
+								};
+								onEdit({ choices: newChoices });
+							}}
+							onEnter={(e) => {
+								const newChoices = [...component.choices!];
+								newChoices.splice(index + 1, 0, {
+									label: "Click to edit label",
+									defaultChecked: false,
+									value: "",
+								});
+								onEdit({ choices: newChoices });
+
+								// wait for re-render and focus the new input
+								const ul = e.currentTarget.parentElement?.parentElement;
+								setTimeout(
+									() =>
+										ul
+											?.querySelector<HTMLInputElement>(
+												":last-child>input[type='text']"
+											)
+											?.focus(),
+									1
+								);
+							}}
+						/>
+					))}
+				</ul>
+			);
+
+		case FormComponentType.Continue:
+			return (
+				<Button variant="primary">
+					<input
+						type="text"
+						value={component.label}
+						className="bg-transparent border-none p-0"
+						placeholder="Click to edit"
+						onChange={(e) => onEdit({ label: e.currentTarget.value })}
+						size={component.label.length}
+					/>
+				</Button>
+			);
+	}
+};
+
+const ComponentPreview: React.FC<{
+	component: FormComponentRow;
+}> = ({ component }) => {
+	switch (component.type) {
+		case FormComponentType.Heading:
+			return <h1 className="font-bold text-xl w-full">{component.label}</h1>;
+		case FormComponentType.Text:
+			return <p>{component.label}</p>;
+		case FormComponentType.TextInput:
+			return <input type="text" value={component.label} className="w-full" />;
+		case FormComponentType.Checkboxes:
+			return (
+				<ul className="divide-y divide-gray-100">
+					{component.choices?.map((choice, index) => (
+						<li key={index} className="flex">
+							<input
+								type="checkbox"
+								className="mr-2"
+								checked={choice.defaultChecked}
+							/>
+							<span>{choice.label}</span>
+						</li>
+					))}
+				</ul>
+			);
+		case FormComponentType.Continue:
+			return <Button variant="primary">{component.label}</Button>;
+	}
 };
 
 const FormComponentRow: React.FC<{
@@ -98,129 +280,28 @@ const FormComponentRow: React.FC<{
 }> = ({ component, index, isEditing, onDelete, onEdit }) => {
 	const [canDrag, setCanDrag] = useState(false);
 
-	return (
-		<div
-			key={index}
-			className={clsx(
-				"flex w-full p-2 my-1",
-				isEditing
-					? "border-2 border-gray-100 cursor-text hover:bg-gray-100"
-					: ""
-			)}
-			draggable={isEditing && canDrag}
-			onDragEnd={(e) => {
-				console.log("TODO - drag & drop", e);
-				setCanDrag(false);
-			}}
-		>
-			{isEditing ? <DragHandle setDragging={setCanDrag} /> : null}
-			{component.type === FormComponentType.Heading ? (
-				<h1 className="font-bold text-xl w-full">
-					{isEditing ? (
-						<input
-							type="text"
-							value={component.label}
-							className="w-full bg-none border-none text-xl"
-							placeholder="Click to edit"
-							onChange={(e) => onEdit({ label: e.currentTarget.value })}
-						/>
-					) : (
-						component.label
-					)}
-				</h1>
-			) : component.type === FormComponentType.Text ? (
-				<p className="w-full">
-					{isEditing ? (
-						<textarea
-							value={component.label}
-							cols={60}
-							className="w-full bg-transparent border-none"
-							onChange={(e) => onEdit({ label: e.currentTarget.value })}
-						/>
-					) : (
-						component.label
-					)}
-				</p>
-			) : component.type === FormComponentType.TextInput ? (
-				<div className="flex flex-col w-full">
-					<input
-						type="text"
-						className="w-full"
-						value={component.label}
-						onChange={(e) => {
-							if (isEditing) onEdit({ label: e.currentTarget.value });
-						}}
-					/>
-				</div>
-			) : component.type === FormComponentType.Checkboxes ? (
-				<div className="flex flex-col w-full">
-					{isEditing ? (
-						<input
-							type="text"
-							className="w-full bg-transparent border-none p-0"
-							value={component.label}
-							onChange={(e) => onEdit({ label: e.currentTarget.value })}
-						/>
-					) : (
-						component.label
-					)}
-					<ul className="my-2 flex flex-col">
-						{component.choices?.map((choice, i) => (
-							<li className="mb-1 flex items-center" key={i}>
-								<input type="checkbox" className="mr-2" />
-								{isEditing ? (
-									<input
-										type="text"
-										className="bg-transparent border-none p-0"
-										value={choice.value}
-										onChange={(e) => {
-											const newChoices = [...(component.choices || [])];
-											newChoices[i] = {
-												...choice,
-												value: e.currentTarget.value,
-											};
-											onEdit({ choices: newChoices });
-										}}
-										onKeyDown={(e) => {
-											console.log("keydown");
+	if (isEditing) {
+		return (
+			<div
+				className="flex w-full p-2 my-1 border-2 border-gray-100 cursor-text hover:bg-gray-100"
+				draggable={isEditing && canDrag}
+				onDragEnd={(e) => {
+					console.log("TODO - drag & drop", e);
+					setCanDrag(false);
+				}}
+			>
+				<DragHandle setDragging={setCanDrag} />
+				<ComponentEditor component={component} onEdit={onEdit} />
+				{component.type !== FormComponentType.Continue ? (
+					<DeleteComponentButton onClick={onDelete} />
+				) : null}
+			</div>
+		);
+	}
 
-											if (e.key === "Enter") {
-												console.log("enter");
-												const newChoices = [...(component.choices || [])];
-												newChoices.splice(i + 1, 0, {
-													label: "...",
-													value: "",
-												});
-												onEdit({ choices: newChoices });
-											}
-										}}
-									/>
-								) : (
-									choice.value
-								)}
-							</li>
-						))}
-					</ul>
-				</div>
-			) : component.type === FormComponentType.Continue ? (
-				<Button variant="primary">
-					{isEditing ? (
-						<input
-							type="text"
-							value={component.label}
-							className="bg-transparent border-none p-0"
-							placeholder="Click to edit"
-							onChange={(e) => onEdit({ label: e.currentTarget.value })}
-							size={component.label.length}
-						/>
-					) : (
-						component.label
-					)}
-				</Button>
-			) : null}
-			{isEditing && component.type !== FormComponentType.Continue ? (
-				<DeleteComponentButton onClick={onDelete} />
-			) : null}
+	return (
+		<div className="flex w-full p-2 my-1">
+			<ComponentPreview component={component} />
 		</div>
 	);
 };
