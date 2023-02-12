fedee7f10822f46f311e6aae8bad782fc8ba2707
Jerome Leclanche <jerome@leclan.ch>
2023-02-12 12:15:12 +0100

    Stub config for integrations

--- src/app/flow-editor/IntegrationNode.tsx
+++ src/app/flow-editor/IntegrationNode.tsx
@@ -1,34 +1,48 @@
 import React, { useState } from "react";
+import { createPortal } from "react-dom";
 import { Handle, NodeProps, Position } from "reactflow";
+import IntegrationSettings from "./IntegrationSettings";
 
 export const IntegrationNode: React.FC<NodeProps> = ({
 	data,
 	isConnectable,
 }) => {
-	const [editorVisible, setEditorVisible] = useState(false);
+	const [settingsVisible, setSettingsVisible] = useState(false);
 
 	const modalRoot = document.getElementById("modal-portal-root");
 	if (!modalRoot) throw new Error("Missing modal portal root");
 
 	return (
-		<div
-			className="flex flex-col bg-red-500 border border-black p-5 rounded-full text-white shadow-md font-semibold group text-center"
-			onClick={() => setEditorVisible(true)}
-		>
-			<Handle
-				type="target"
-				position={Position.Top}
-				isConnectable={isConnectable}
-				style={{ visibility: "hidden" }}
-			/>
-			{data?.label}
-			<Handle
-				type="source"
-				position={Position.Bottom}
-				isConnectable={isConnectable}
-				style={{ visibility: "hidden" }}
-			/>
-		</div>
+		<>
+			{settingsVisible
+				? createPortal(
+						<IntegrationSettings
+							name={data?.label}
+							save={() => setSettingsVisible(false)}
+						/>,
+						modalRoot
+				  )
+				: null}
+
+			<div
+				className="flex flex-col bg-red-500 border border-black p-5 rounded-full text-white shadow-md font-semibold group text-center"
+				onClick={() => setSettingsVisible(true)}
+			>
+				<Handle
+					type="target"
+					position={Position.Top}
+					isConnectable={isConnectable}
+					style={{ visibility: "hidden" }}
+				/>
+				{data?.label}
+				<Handle
+					type="source"
+					position={Position.Bottom}
+					isConnectable={isConnectable}
+					style={{ visibility: "hidden" }}
+				/>
+			</div>
+		</>
 	);
 };
 
