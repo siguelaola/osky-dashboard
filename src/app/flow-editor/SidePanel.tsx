0ead09b3e5711cfd60908dd4ebc913ec602304b2
Steve Brand <thatfanficguy@protonmail.com>
2023-03-12 00:42:45 +0100

    Make side panel tab state more legible on hover

--- src/app/flow-editor/SidePanel.tsx
+++ src/app/flow-editor/SidePanel.tsx
@@ -165,7 +165,7 @@ export const SidePanel = () => {
 							className={clsx(
 								tab === activeTab
 									? "bg-indigo-100 text-indigo-700"
-									: "text-gray-500 hover:text-gray-700",
+									: "text-gray-500 hover:text-gray-700 hover:bg-gray-100",
 								"rounded-md px-3 py-2 text-sm font-medium"
 							)}
 							onClick={() => setActiveTab(tab)}
