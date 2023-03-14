1a6bb38f4fcae20233ab966ab232d2cd646d6146
Steve Brand <thatfanficguy@protonmail.com>
2023-03-14 23:37:19 +0100

    Pull footer to the side (center in main area)

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,5 +1,5 @@
 const Footer = () => (
-	<footer className="absolute w-full bottom-0 text-center text-gray-400">
+	<footer className="absolute w-full bottom-0 text-center text-gray-400 pl-64">
 		&copy; {new Date().getFullYear()} Ola Financial Technologies, Inc.
 	</footer>
 );
