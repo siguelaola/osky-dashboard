f92a7c5e8506f34d0d252219bde3dac03cef8e51
Steve Brand <thatfanficguy@protonmail.com>
2023-03-14 23:36:54 +0100

    Remove unused navigation object in footer

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,10 +1,3 @@
-const navigation = {
-	main: [
-		{ name: "Features", href: "#" },
-		{ name: "Contact", href: "mailto:contact@siguelaola.com" },
-	],
-};
-
 const Footer = () => (
 	<footer className="absolute w-full bottom-0 text-center text-gray-400">
 		&copy; {new Date().getFullYear()} Ola Financial Technologies, Inc.
