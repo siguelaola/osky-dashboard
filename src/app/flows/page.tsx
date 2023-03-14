d5ade28e1f70bae77a333a33093cca5455ccf42a
Steve Brand <thatfanficguy@protonmail.com>
2023-03-14 23:37:54 +0100

    Put "Create new flow" on the same line as "Flows"

--- src/app/flows/page.tsx
+++ src/app/flows/page.tsx
@@ -3,13 +3,15 @@ import FlowsList from "./FlowsList";
 
 const Page = () => {
 	return (
-		<div className="p-3">
-			<h1 className="font-semibold text-3xl mb-5">Flows</h1>
-			<aside className="my-3 flex">
-				<ButtonLink href="/flows/new" variant="primary" className="self-end">
-					Create new flow
-				</ButtonLink>
-			</aside>
+		<div className="flex flex-col p-3 bg-slate-100 h-screen overflow-auto pb-8">
+			<div className="flex items-center mb-2">
+				<h1 className="font-semibold text-3xl self-start">Flows</h1>
+				<aside className="flex gap-x-2 ml-auto">
+					<ButtonLink href="/flows/new" variant="primary" className="self-end">
+						Create new flow
+					</ButtonLink>
+				</aside>
+			</div>
 
 			<FlowsList />
 		</div>
