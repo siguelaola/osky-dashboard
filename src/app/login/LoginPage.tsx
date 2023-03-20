6dc70408a9849acdd7daf3fd51836b709e9c2385
Jerome Leclanche <jerome@leclan.ch>
2023-03-20 15:03:54 +0100

    Split login page client component

---
+++ src/app/login/LoginPage.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { useSupabase } from "../(components)/supabase/SupabaseProvider";
+
+const LoginPage: React.FC<{}> = () => {
+	const { supabase } = useSupabase();
+	return (
+		<Auth
+			supabaseClient={supabase}
+			appearance={{ theme: ThemeSupa }}
+			redirectTo="/flows"
+		/>
+	);
+};
+
+export default LoginPage;
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,9 @@
-"use client";
+import LoginPage from "./LoginPage";
 
-import { Auth } from "@supabase/auth-ui-react";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
-import { useSupabase } from "../(components)/supabase/SupabaseProvider";
-
-const Login: React.FC<{}> = () => {
-	const { supabase } = useSupabase();
+const Login = () => {
 	return (
 		<div className="max-w-md p-8 m-auto mt-4">
-			<Auth
-				supabaseClient={supabase}
-				appearance={{ theme: ThemeSupa }}
-				redirectTo="/flows"
-			/>
+			<LoginPage />
 		</div>
 	);
 };
