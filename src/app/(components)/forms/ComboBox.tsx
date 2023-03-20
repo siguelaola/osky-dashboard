442562595413fe9dc2fce4d80692b78d14515d2e
Jerome Leclanche <jerome@leclan.ch>
2023-03-20 15:34:10 +0100

    temp fix

--- src/app/(components)/forms/ComboBox.tsx
+++ src/app/(components)/forms/ComboBox.tsx
@@ -50,7 +50,7 @@ const ComboBox: React.FC<{
 			className="flex flex-auto"
 		>
 			<div className="relative mt-1 flex flex-grow">
-				<Combobox.Input<any, ComboBoxEntry>
+				<Combobox.Input<any, any>
 					className="w-full rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"
 					onChange={(event) => setQuery(event.target.value)}
 					placeholder={placeholder}
--- src/app/flow-editor/FlowPreview.tsx
+++ src/app/flow-editor/FlowPreview.tsx
@@ -1,9 +1,6 @@
 import { StateMachine } from "osky-ui";
 import { useReactFlow } from "reactflow";
 
-import type { Edge, Node } from "reactflow";
-import type { NodeScreen } from "osky-ui/dist/interfaces/types";
-
 const FlowPreview = () => {
 	const flow = useReactFlow();
 	const nodes = flow.getNodes();
@@ -11,7 +8,7 @@ const FlowPreview = () => {
 
 	return (
 		<div className="bg-white h-full w-[min(calc(100%-20rem),70rem)] z-10 shadow-lg rounded-lg p-3 mr-3">
-			<StateMachine session={{ nodes: nodes as NodeScreen[], edges }} />
+			<StateMachine session={{ nodes: nodes as any, edges }} />
 		</div>
 	);
 };
