c0a5d2a5785eb580ceaf45e47ca6bae3c4be7fae
Jerome Leclanche <jerome@leclan.ch>
2023-03-20 15:28:18 +0100

    Hack to redirect on login
    Fixes #33

--- src/app/login/LoginPage.tsx
+++ src/app/login/LoginPage.tsx
@@ -1,15 +1,36 @@
 "use client";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { useEffect } from "react";
 import { useSupabase } from "../(components)/supabase/SupabaseProvider";
 
 const LoginPage: React.FC<{}> = () => {
 	const { supabase } = useSupabase();
+
+	// XXX Hack because <Auth/> is broken with appDir
+	useEffect(() => {
+		const submitButton = document
+			.getElementById("auth-sign-in")
+			?.querySelector<HTMLButtonElement>("button[type=submit]");
+
+		if (submitButton) {
+			submitButton.onclick = () => {
+				setTimeout(async () => {
+					const userResponse = await supabase.auth.getUser();
+					console.log(userResponse);
+					if (!userResponse.error) {
+						window.location.href = "/flows";
+					}
+				}, 1000);
+			};
+		}
+	});
+
 	return (
 		<Auth
 			supabaseClient={supabase}
 			appearance={{ theme: ThemeSupa }}
-			redirectTo="flows"
+			redirectTo="/flows"
 			providers={[]}
 		/>
 	);
